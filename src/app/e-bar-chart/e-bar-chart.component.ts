6e8c46c97e970745408ae21c1468db086dc3de3f
Amit Kumar <amit.kumar3@mail.vinove.com>
2022-10-17 18:56:21 +0530

    chart

--- src/app/e-bar-chart/e-bar-chart.component.ts
+++ src/app/e-bar-chart/e-bar-chart.component.ts
@@ -12,24 +12,33 @@ export class EBarChartComponent implements OnInit {
   ngOnInit(): void {}
 
   public chartOption: EChartsOption = {
+    title: {
+      text: 'Analogue:'
+    },
     legend: {},
-    xAxis: { type: 'category' },
-    yAxis: {},
+    xAxis: { type: 'category'},
+    yAxis: {      type: 'value',
+    axisLabel: {
+      formatter: '${value}'
+    },
+    min:3000
+  },
     dataset: {
       source: [
         ['product', '2015', '2016', '2017', '2018', '2019', '2020'],
-        ['United Kingdom', 43.3, 85.8, 93.7,32.1,57.6],
-        ['France', 83.1, 73.4, 55.1,34.1,26.2],
-        ['Germany', 86.4, 65.2, 82.5,45.1,23.2],
+        ['United Kingdom', 3800, 5200, 3904,4000,5200],
+        ['France', 4381, 7324, 5531,4401,6602],
+        ['Germany', 5314, 6562, 7295,4531,7352],
       ],
     },
     series: [
-      { type: 'bar', barGap: '20%', barCategoryGap: '30%',barWidth: '8%' },
+      { type: 'bar', barGap: '20%', barCategoryGap: '20%',barWidth: '8%' },
       { type: 'bar' , barWidth: '8%' },
       { type: 'bar',barWidth: '8%'},
       { type: 'bar',barWidth: '8%'},      
       { type: 'bar',barWidth: '8%'},
       { type: 'bar',barWidth: '8%'},
+      
     ],
   };
 }
